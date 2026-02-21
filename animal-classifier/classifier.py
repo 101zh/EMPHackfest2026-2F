f033ab056ec0b2e8aa184935b0fbc9b5e6d489b5
Pranav Dasan <pranavdasan8@gmail.com>
2026-02-21 14:37:33 -0800

    Classifier returns predicted animal

---
+++ animal-classifier/classifier.py
@@ -0,0 +1,47 @@
+import subprocess
+from pathlib import Path
+import sys
+import json
+
+# Paths for images and classifier output
+image_folder = Path(r"..\speciesnet-input")
+output_file = Path(r".\\output.json")
+
+
+def getAnimal() :
+    # Delete old output.json (SpeciesNet throws error otherwise)
+    if output_file.exists():
+        output_file.unlink()
+        print(f"Deleted old file: {output_file}")
+    else:
+        print("No old output file to delete.")
+
+    # Run SpeciesNet classifier
+    command = [
+        sys.executable,  # Use current Python interpreter
+        "-m",
+        "speciesnet.scripts.run_model",
+        "--classifier_only", # Set to classifier only mode
+        "--folders",
+        str(image_folder), # 
+        "--predictions_json",
+        str(output_file),
+    ]
+
+    print("Running classifier...")
+    subprocess.run(command, input="y\n", text=True)
+    print("Classifier finished!")
+
+    # Read classification json
+    with open(str(output_file)) as json_data:
+        output_json = json.load(json_data)
+        json_data.close()
+
+    # Gets the most confident animal classification data
+    classification = output_json["predictions"][0]["classifications"]["classes"][0] 
+
+    # Gets the animal predicted from classification data
+    animal_predicted = classification.split(";")[-1]
+    print(animal_predicted)
+
+    return animal_predicted
