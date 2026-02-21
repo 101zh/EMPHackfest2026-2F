c8538dd27965eba806b138886e8702ce7114fec7
101zh <67253838+101zh@users.noreply.github.com>
2026-02-21 11:45:12 -0800

    add image send code

--- animal-detector/script.js
+++ animal-detector/script.js
@@ -1,5 +1,29 @@
 const fileInput = document.getElementById("animal-pic-file");
 
 window.addEventListener('paste', e => {
-  fileInput.files = e.clipboardData.files;
+    fileInput.files = e.clipboardData.files;
 });
+
+document.getElementById("send").addEventListener("click", function () {
+    if (fileInput.files && fileInput.files[0]) {
+        upload(fileInput.files[0]);
+    }
+});
+
+function upload(file) {
+    console.log(file)
+    var formdata = new FormData();
+    formdata.append("test", file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:5000/", true);
+    xhr.onload = function () {
+        if (this.status = 200) {
+            console.log(this.response);
+        } else {
+            console.error(xhr);
+        }
+        alert(this.response);
+    };
+    xhr.send(formdata);
+}
