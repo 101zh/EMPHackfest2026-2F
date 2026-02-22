3086f0dcca6afb092eaba6cd4f3477ea2f46204c
GuestUser1000101 <ryanzou2008@gmail.com>
2026-02-22 14:20:21 -0800

    Change name of database

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_db():
-    db_path = os.path.join(BASE_DIR, "animals.db")
+    db_path = os.path.join(BASE_DIR, "user_info.db")
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
